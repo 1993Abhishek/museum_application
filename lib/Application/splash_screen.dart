d1588dfd30e0fbac56b50d035b67b90288789f95
Abhishek-ncrts <abhishek.ghoshal.ncrts@gmail.com>
2021-07-27 11:28:55 +0530

    26/7/2021 4:24 pm loading

--- lib/Application/splash_screen.dart
+++ lib/Application/splash_screen.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:museum_application/helper/appcolor.dart';
 import 'package:museum_application/router.dart';
+import 'package:museum_application/utility/app_dimen.dart';
 import 'package:museum_application/utility/size_config.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -12,10 +14,48 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  double finalWidth=300;
+
+  Widget loadingContainer() {
+    return Container(
+      height: hDimen(30),
+      width: hDimen(300),
+      decoration: BoxDecoration(
+        color:Colors.white /*Color(0xFFf2f2f2)*/,
+        borderRadius: BorderRadius.circular(hDimen(20)),
+        border: Border.all(
+          color: Color(0xFFf2f2f2),
+          width: 0.0,
+        ),
+      ),
+      child: AnimatedPadding(
+        duration: Duration(seconds: 3,),
+        padding: EdgeInsets.only(right:finalWidth),
+        child: AnimatedContainer(
+          duration: Duration(seconds: 3),
+          height: hDimen(2),
+          width: 1,
+          decoration: BoxDecoration(
+            color: Colors.green/*AppColor.colorPrimary*/,
+            borderRadius: BorderRadius.circular(hDimen(20)),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   void initState() {
-    // TODO: implement initState
-    Timer(Duration(seconds: 3), () {
+
+    Timer(Duration(seconds: 1), () {
+      while (finalWidth!=0) {
+        setState(() {
+          finalWidth = finalWidth-1;
+        });
+      }
+    });
+
+    Timer(Duration(seconds: 4), () {
       Navigator.pushReplacementNamed(
         context,
         MuseumRouter.ROUTE_HOME,
@@ -23,11 +63,14 @@ class _SplashScreenState extends State<SplashScreen> {
     });
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Scaffold(
-      body:  Container(
+      body: Stack(
+        children: [
+          Container(
             decoration: BoxDecoration(
               image: DecorationImage(
                 image: AssetImage(
@@ -37,6 +80,17 @@ class _SplashScreenState extends State<SplashScreen> {
               ),
             ),
           ),
+          Padding(
+            padding: EdgeInsets.only(
+              bottom: hDimen(20),
+            ),
+            child: Align(
+              child: loadingContainer(),
+              alignment: Alignment.bottomCenter,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
